4e6b6122ed674ab20acda9aa610f2b5d343930d0
Richard Hubbard <richard.hubbard@armadry.co.uk>
2024-12-02 10:33:38 +0000

    Day 2

--- lib/days/day2/README.md
+++ lib/days/day2/README.md
@@ -1,2 +1,9 @@
-# Day 1
+# Day 2
 
+I told you I wasn't ready! That took longer than it should have.
+
+* I initially missed the fact that values could be decreasing as well as increasing.
+* I then got a little hung up on the `O()` of part 2 without even just trying the naïve solution first.
+* I also forgot that, in the case where the first value is the one that's ignored, that might change the expected direction of change.
+
+Got there in the end though.
--- lib/days/day2/index.ts
+++ lib/days/day2/index.ts
@@ -4,15 +4,52 @@ import { Day } from '..';
 
 export const meta: Day['meta'] = {};
 
+function isSafe(report: number[]) {
+  const direction = Math.sign(report[1] - report[0]);
+
+  if (direction === 0) {
+    return false;
+  }
+
+  return report.slice(1).reduce(
+    (safe, value, i) => {
+      const diff = (value - report[i]) * direction;
+      return safe && (diff >= 1) && (diff <= 3);
+    },
+    true
+  );
+}
+
 export function part1() {
-  return 1;
+  const reports = cleanAndParse(input, s => cleanAndParse(s, Number, { separator: ' ' }));
+
+  const safeReports = reports.filter(report => isSafe(report));
+
+  return safeReports.length;
 }
 
 export function part2() {
-  return 2;
+  const reports = cleanAndParse(input, s => cleanAndParse(s, Number, { separator: ' ' }));
+
+  const safeReports = reports.filter(report => {
+    const len = report.length;
+    for (let i = 0; i < len; i++) {
+      const testReport = [...report];
+      testReport.splice(i, 1);
+
+      if (isSafe(testReport)) {
+        return true;
+      }
+    }
+    return false;
+  });
+
+  console.log(safeReports);
+
+  return safeReports.length;
 }
 
-// export const answers = [
-//   54601,
-//   54078
-// ];
+export const answers = [
+  572,
+  612
+];
