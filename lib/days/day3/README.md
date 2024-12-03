3ed92b4dd14c6a3df3f0a5674bada3cd4367cc35
Richard Hubbard <richard.hubbard@armadry.co.uk>
2024-12-03 10:40:04 +0000

    Day 3

--- lib/days/day3/README.md
+++ lib/days/day3/README.md
@@ -1,2 +1,7 @@
 # Day 3
 
+Ah Regex! `mul\(\d{1,3},\d{1,3}\)`. My first attempt was "no code", just see how many times VSCode found that regex. 726 - too low! Ah but RTFQ, it's not just count them but do them...
+
+Then, for the first time ever, the input doesn't fit in a JS template string. Because I couldn't be bothered to enhance my framework with async file reading, I fudged in the console to get a JS compliant string expression.
+
+
--- lib/days/day3/index.ts
+++ lib/days/day3/index.ts
@@ -1,18 +1,55 @@
-import { eg, input } from './input';
+import { eg, eg2, input } from './input';
 import { cleanAndParse } from '../../utils';
 import { Day } from '..';
 
 export const meta: Day['meta'] = {};
 
+function evalMul(s: string) {
+  const [a, b] = s
+    .replace('mul(', '')
+    .replace(')', '')
+    .split(',')
+    .map(Number);
+
+  return a * b;
+}
+
 export function part1() {
-  return 1;
+  const regex = /mul\(\d{1,3},\d{1,3}\)/g;
+
+  const ex = input.match(regex);
+
+  const ans = ex?.reduce((acc, curr) => acc + evalMul(curr), 0);
+
+  return ans;
 }
 
 export function part2() {
-  return 2;
+  const regex = /(mul\(\d{1,3},\d{1,3}\))|(do\(\))|(don't\(\))/g;
+  const ex = input.match(regex);
+
+  let doMul = true;
+
+  const ans = ex?.reduce((acc, curr) => {
+    if (curr === "do()") {
+      doMul = true;
+      return acc;
+    }
+    if (curr === "don't()") {
+      doMul = false;
+      return acc;
+    }
+    if (doMul) {
+      return acc + evalMul(curr);
+    }
+    return acc;
+  }, 0);
+
+
+  return ans;
 }
 
-// export const answers = [
-//   54601,
-//   54078
-// ];
+export const answers = [
+  188116424,
+  104245808
+];
