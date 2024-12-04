c19958ec51a20ad8843f4a5d81d21a0c79d1515f
Richard Hubbard <richard.hubbard@armadry.co.uk>
2024-12-04 10:51:12 +0000

    Day 4

--- lib/days/day4/README.md
+++ lib/days/day4/README.md
@@ -1,2 +1,5 @@
 # Day 4
 
+Not too challenging, but needed grid functions don't have utilities for. Part 1 takes ~30ms. I mean, that's _OK_, but I wonder if there's a well known algorithm I should be using.
+
+Looked at the global leaderboard today. The #1 for part one did it in 19 seconds. The rules say don't use AI!!!!!
--- lib/days/day4/index.ts
+++ lib/days/day4/index.ts
@@ -1,18 +1,82 @@
 import { eg, input } from './input';
-import { cleanAndParse } from '../../utils';
+import { cleanAndParse, CoordinateRange, coordinates, neighbours, simpleRange } from '../../utils';
 import { Day } from '..';
 
 export const meta: Day['meta'] = {};
 
+function xmasesAt(grid: string[][], range: CoordinateRange, row: number, col: number) {
+  const cell = grid[row][col];
+
+  let found = 0;
+
+  if (cell === 'X') {
+    for (const [nr, nc] of neighbours([row, col], range)) {
+      if (grid[nr][nc] === 'M') {
+        const dr = nr - row;
+        const dc = nc - col;
+
+        if ((grid[nr + dr]?.[nc + dc] === 'A') && (grid[nr + 2 * dr]?.[nc + 2 * dc] === 'S')) {
+          found++;
+        }
+      }
+    }
+  }
+
+  return found;
+}
+type Counter = typeof xmasesAt;
+
+function masXesAt(grid: string[][], range: CoordinateRange, row: number, col: number) {
+  const cell = grid[row][col];
+
+  let found = 0;
+
+  if (cell === 'A') {
+    if (testMasX(grid, row, col)) {
+      found++;
+    }
+  }
+
+  return found;
+}
+
+function testMasX(grid: string[][], row: number, col: number) {
+  const corners = [[-1, -1], [-1, 1], [1, -1], [1, 1]].map(([dr, dc]) => grid[row + dr]?.[col + dc]).join('');
+  const valid = [
+    'MMSS',
+    'SSMM',
+    'MSMS',
+    'SMSM'
+  ]
+
+  return valid.includes(corners);
+}
+
+export function doPart(data: string, counter: Counter) {
+  const grid = cleanAndParse(data, l => Array.from(l));
+  let ans = 0;
+
+  const height = grid.length;
+  const width = grid[0].length;
+
+  const range = simpleRange([height, width]);
+
+  for (const [row, col] of coordinates(range)) {
+    ans += counter(grid, range, row, col);
+  }
+
+  return ans;
+}
+
 export function part1() {
-  return 1;
+  return doPart(input, xmasesAt);
 }
 
 export function part2() {
-  return 2;
+  return doPart(input, masXesAt);
 }
 
-// export const answers = [
-//   54601,
-//   54078
-// ];
+export const answers = [
+  2462,
+  1877
+];
