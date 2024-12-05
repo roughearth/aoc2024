57239fa971747ed1af14a24c3f9999c08053e9fc
Richard Hubbard <richard.hubbard@armadry.co.uk>
2024-12-05 10:37:17 +0000

    Day 5

--- lib/days/day5/README.md
+++ lib/days/day5/README.md
@@ -1,2 +1,3 @@
 # Day 5
 
+Still being fairly straightforward, even if I'm taking my time. FOr soem reason it took me a few minutes to think of `sort` for part 2.
--- lib/days/day5/index.ts
+++ lib/days/day5/index.ts
@@ -4,15 +4,89 @@ import { Day } from '..';
 
 export const meta: Day['meta'] = {};
 
+function parseData(input: string) {
+  const rules = new Map<number, number[]>();
+  const updates = new Array<number[]>();
+
+  cleanAndParse(input, l => {
+    if (l.includes('|')) {
+      const [before, after] = l.split('|').map(Number);
+      if (!rules.has(before)) {
+        rules.set(before, []);
+      }
+      rules.get(before)!.push(after);
+    }
+    else if (l.includes(',')) {
+      updates.push(l.split(',').map(Number));
+    }
+  });
+
+  return { rules, updates };
+}
+
+function analyse(data: string) {
+  const { rules, updates } = parseData(data);
+
+  const good: number[][] = [];
+  const bad: number[][] = [];
+
+  for (const update of updates) {
+    const l = update.length;
+    let isGood = true;
+
+    for (let i = 0; i < l; i++) {
+      if (isGood) {
+        const value = update[i];
+        const forbidden = rules.get(value);
+        const preceding = update.slice(0, i);
+
+        for (const test of preceding) {
+          if (forbidden?.includes(test)) {
+            isGood = false;
+            break;
+          }
+        }
+      }
+    }
+    if (isGood) {
+      good.push(update);
+    }
+    else {
+      bad.push(update);
+    }
+  }
+
+  return { good, bad, rules };
+}
+
+function getScore(updates: number[][]) {
+  return updates.reduce((a, b) => a + b[Math.floor(b.length / 2)], 0);
+}
+
+function reorder(update: number[], rules: Map<number, number[]>) {
+  update.sort((a, b) => {
+    const rule = rules.get(a) ?? [];
+    if (rule.includes(b)) {
+      return -1;
+    }
+    return 1
+  });
+}
+
 export function part1() {
-  return 1;
+  const { good } = analyse(input);
+
+  return getScore(good);
 }
 
 export function part2() {
-  return 2;
+  const { bad, rules } = analyse(input);
+  bad.forEach(b => reorder(b, rules));
+
+  return getScore(bad);
 }
 
-// export const answers = [
-//   54601,
-//   54078
-// ];
+export const answers = [
+  5087,
+  4971
+];
