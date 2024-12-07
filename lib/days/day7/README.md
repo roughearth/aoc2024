6e5379c51ab9e125e076a5a9d2ab29403a031bd7
Richard Hubbard <richard.hubbard@armadry.co.uk>
2024-12-07 10:47:51 +0000

    Day 7

--- lib/days/day7/README.md
+++ lib/days/day7/README.md
@@ -1,2 +1,8 @@
 # Day 7
 
+Not much option but to brute force all the combinations. OK for part 1, but part 2 got much bigger.
+
+My first attempt took 17 seconds. I got it down to 2½ by optimising the calculation.
+
+* First attempt did string analysis on the base 2 or 3 number what represented the operation set. I got it down to 7½ seconds by using modular maths instead.
+* Then I decided to look at the "concatenation" operation. The first attempt was to use string concatenation, and `parseInt` the result. I got it down to 2½ seconds by using logs to work out what to multiply by.
--- lib/days/day7/index.ts
+++ lib/days/day7/index.ts
@@ -2,17 +2,67 @@ import { eg, input } from './input';
 import { cleanAndParse } from '../../utils';
 import { Day } from '..';
 
-export const meta: Day['meta'] = {};
+export const meta: Day['meta'] = {
+  manualStart: false,
+};
+
+function parseData(data: string) {
+  return cleanAndParse(data, l => {
+    const [target, rest] = l.split(': ');
+    const numbers = rest.split(" ").map(n => parseInt(n, 10));
+    return {
+      target: parseInt(target, 10),
+      numbers
+    };
+  });
+}
+
+function tryCalc(numbers: number[], ops: number, opCount: number, len: number) {
+  return numbers.reduce((acc, n, i) => {
+    const op = ops % opCount;
+    ops = Math.floor(ops / opCount);
+
+    if (op === 1) {
+      return acc + n;
+    }
+    if (op === 2) {
+      const s = Math.ceil(Math.log10(n + 1));
+      return (acc * (10 ** s)) + n;
+    }
+    return acc * n;
+  });
+}
+
+export function doPart(opCount: number, dataIn: string) {
+  const data = parseData(dataIn);
+
+  const found: number[] = [];
+
+  loop:
+  for (const { target, numbers } of data) {
+    const len = numbers.length - 1;
+    const ops = opCount ** len;
+
+    for (let i = 0; i < ops; i++) {
+      if (target === tryCalc(numbers, i, opCount, len)) {
+        found.push(target);
+        continue loop;
+      }
+    }
+  }
+
+  return found.reduce((acc, n) => acc + n, 0);
+}
 
 export function part1() {
-  return 1;
+  return doPart(2, input);
 }
 
 export function part2() {
-  return 2;
+  return doPart(3, input);
 }
 
-// export const answers = [
-//   54601,
-//   54078
-// ];
+export const answers = [
+  1582598718861,
+  165278151522644
+];
